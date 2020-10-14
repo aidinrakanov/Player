c64091550d36139307ef7a8231e63fe73f69d56f
aidinrakanov <altynaikanatbekova@gmail.com>
2020-10-14 21:46:36 +0600

    player done

--- app/src/main/java/com/example/player/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/example/player/ui/player/PlayerActivity.kt
@@ -2,16 +2,85 @@ package com.example.player.ui.player
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import com.example.player.R
 import com.example.player.model.PlayerModel
+import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
+
+    private var flag: Boolean = true
+    private lateinit var handler : Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+        setUpListeners()
+        seekbarChange()
+        initial()
+
     }
+
+    private fun initial() {
+        seekbar.max= mediaPlayer.duration
+        val handler = Handler()
+        handler.postDelayed(object : Runnable{
+            override fun run() {
+                try {
+                    seekbar.progress = mediaPlayer.currentPosition
+                    handler.postDelayed(this, 1000)
+                } catch (e : Exception){
+                    seekbar.progress = 0
+                }
+
+            }
+        },0)
+    }
+
+
+    private fun seekbarChange() {
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if (p2)mediaPlayer.seekTo(p1)
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+    }
+
+    private fun setUpListeners() {
+        play.setOnClickListener {
+            if (flag){
+                mediaPlayer.pause()
+                play.setImageResource(R.drawable.ic_play)
+            }else{
+                mediaPlayer.start()
+                play.setImageResource(R.drawable.ic_pause)
+            }
+            flag = !flag
+        }
+    }
+
+
+    val url = item?.url
+    val mediaPlayer: MediaPlayer = MediaPlayer().apply {
+        setAudioAttributes(
+            AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build()
+        )
+        setDataSource(url)
+        prepare()
+        start()
+    }
+
 
     companion object {
         private var item: PlayerModel? = null
